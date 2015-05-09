56ed96eaea5b3f10f8044388a8a88222e74257c2
dwayhs <danielwayhs@gmail.com>
2015-05-08 21:26:33 -0300

    Added unit test structure for all models

---
+++ spec/factories/projects.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :project do
+    name "MyString"
+description "MyString"
+scm_type "MyString"
+scm_reference "MyString"
+  end
+
+end
---
+++ spec/factories/technologies.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :technology do
+    
+  end
+
+end

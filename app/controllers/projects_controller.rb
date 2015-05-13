47f54b535b4db89517d5212b0b3ed03a1c1d1c13
dwayhs <danielwayhs@gmail.com>
2015-05-13 16:33:42 -0300

    Added members to the project form

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -70,6 +70,12 @@ class ProjectsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def project_params
-    params.require(:project).permit(:name, :description, :scm_type, :scm_reference, :image)
+    params.require(:project).permit(
+      :name,
+      :description,
+      :scm_type,
+      :scm_reference,
+      :image,
+      {:member_ids => []})
   end
 end

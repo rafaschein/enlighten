5b420d5420fdfc6f8e681394657ed2562059d128
dwayhs <daniel@wayhs.me>
2015-07-09 21:01:50 -0300

    Removing respond_to :son form the controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,13 +2,11 @@ class ClientsController < ApplicationController
   before_action :set_client, only: [:show, :edit, :update, :destroy]
 
   # GET /clients
-  # GET /clients.json
   def index
     @clients = Client.all
   end
 
   # GET /clients/1
-  # GET /clients/1.json
   def show
   end
 
@@ -22,43 +20,30 @@ class ClientsController < ApplicationController
   end
 
   # POST /clients
-  # POST /clients.json
   def create
     @client = Client.new(client_params)
 
-    respond_to do |format|
-      if @client.save
-        format.html { redirect_to @client, notice: 'Client was successfully created.' }
-        format.json { render :show, status: :created, location: @client }
-      else
-        format.html { render :new }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.save
+      redirect_to @client, notice: 'Client was successfully created.'
+    else
+      render :new
     end
   end
 
   # PATCH/PUT /clients/1
-  # PATCH/PUT /clients/1.json
   def update
-    respond_to do |format|
-      if @client.update(client_params)
-        format.html { redirect_to @client, notice: 'Client was successfully updated.' }
-        format.json { render :show, status: :ok, location: @client }
-      else
-        format.html { render :edit }
-        format.json { render json: @client.errors, status: :unprocessable_entity }
-      end
+    if @client.update(client_params)
+      redirect_to @client, notice: 'Client was successfully updated.'
+    else
+      render :edit
     end
   end
 
   # DELETE /clients/1
-  # DELETE /clients/1.json
   def destroy
     @client.destroy
-    respond_to do |format|
-      format.html { redirect_to clients_url, notice: 'Client was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+
+    redirect_to clients_url, notice: 'Client was successfully destroyed.'
   end
 
   private

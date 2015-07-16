43c379484720333bde5c7ebce7ee0fb7ac70b3b3
dwayhs <daniel@wayhs.me>
2015-07-16 14:58:20 -0300

    Added some social network icons and paths

--- spec/models/social_link_spec.rb
+++ spec/models/social_link_spec.rb
@@ -22,10 +22,6 @@ RSpec.describe SocialLink, type: :model do
   it { expect(subject).to have_db_column(:updated_at) }
   it { expect(subject).to belong_to(:person) }
 
-  before do
-    allow(Rails.application).to receive(:config_for).and_return(yml_configuration)
-  end
-
   let(:yml_configuration) do
     [
       { 'provider' => 'github', 'url' => 'https://github.com/#identifier' },
@@ -41,6 +37,10 @@ RSpec.describe SocialLink, type: :model do
 
   describe '.provider_url_configuration' do
     subject { SocialLink.provider_url_configuration(provider) }
+
+    before do
+      allow(Rails.application).to receive(:config_for).and_return(yml_configuration)
+    end
 
     context 'when provider is github' do
       let(:provider) { 'github' }
@@ -58,6 +58,10 @@ RSpec.describe SocialLink, type: :model do
   describe '.configuration' do
     subject { SocialLink.configuration }
 
+    before do
+      allow(Rails.application).to receive(:config_for).and_return(yml_configuration)
+    end
+
     it do
       expect(subject). to eq([
         { provider: 'github', url: 'https://github.com/#identifier' },
@@ -69,6 +73,18 @@ RSpec.describe SocialLink, type: :model do
   describe '#url' do
     subject { create :social_link, provider: 'github', identifier: 'CWISoftware' }
 
+    before do
+      allow(Rails.application).to receive(:config_for).and_return(yml_configuration)
+    end
+
     it { expect(subject.url).to eq('https://github.com/CWISoftware') }
+  end
+
+  describe 'social link icons' do
+    it 'has all configured social links icons' do
+      SocialLink.available_providers.each do |provider|
+        expect(Rails.root.join('app', 'assets', 'images', 'social', "#{provider}.png")).to exist
+      end
+    end
   end
 end

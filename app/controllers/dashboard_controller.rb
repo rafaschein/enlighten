84c81dfbc42fa85b28f5e41e682938c679d6e7f9
John Voloski <johnvoloski@cwi.com.br>
2015-12-11 14:37:40 -0200

    implements policy permission in dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    #authorize :dashboard, :index?
+    authorize :dashboard, :index?
 
     @technologies = Technology.all
     @people = Person.all
--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+  let(:permission_acls) do
+    [
+      build(:permission_acl, model: 'dashboard', action: 'index')
+    ]
+  end
+  let(:permission_roles) { [double(permission_acls: permission_acls)] }
+  let(:current_user)     { create :user }
+
   before do
-    allow(controller).to receive(:authenticate_user!).and_return true
+    sign_in current_user
+    allow(controller).to receive(:authenticate_user!) { true }
+    allow(controller.current_user).to receive(:permission_roles) { permission_roles }
   end
 
   describe 'GET #index' do

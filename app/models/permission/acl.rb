15e6f9611e99ddc8f53b34ffb201f867b0e134d9
John Voloski <johnvoloski@cwi.com.br>
2015-12-11 14:40:33 -0200

    fix permission acl lint and create spec

--- app/models/permission/acl.rb
+++ app/models/permission/acl.rb
@@ -10,11 +10,17 @@
 #  updated_at         :datetime         not null
 #
 
-class Permission::Acl < ActiveRecord::Base
-  has_paper_trail
+module Permission
+  class Acl < ActiveRecord::Base
+    has_paper_trail
 
-  validates :model, presence: true
-  validates :action, presence: true
+    validates :model, presence: true
+    validates :action, presence: true
 
-  belongs_to :permission_role, class_name: Permission::Role.name, foreign_key: 'permission_role_id'
+    belongs_to :permission_role, class_name: Permission::Role.name, foreign_key: 'permission_role_id'
+
+    def self.table_name_prefix
+      'permission_acl_'
+    end
+  end
 end
--- spec/models/permission/acl_spec.rb
+++ spec/models/permission/acl_spec.rb
@@ -20,4 +20,10 @@ RSpec.describe Permission::Acl, type: :model do
 
   it { expect(subject).to validate_presence_of(:model) }
   it { expect(subject).to validate_presence_of(:action) }
+
+  describe '.table_name_prefix' do
+    it 'returns table name prefixed' do
+      expect(subject.table_name_prefix).to eq('permission_acl_')
+    end
+  end
 end

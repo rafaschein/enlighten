814f3d5882dcbd15e8e77e55be406428763c430d
John Voloski <johnvoloski@cwi.com.br>
2015-12-17 14:38:26 -0200

    adding devise ldap authenticatable strategy configuration

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,6 +1,14 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
+  # ==> LDAP Configuration
+  if Figaro.env.ldap_enabled
+    config.ldap_logger = true
+    config.ldap_create_user = true
+    config.ldap_config = "#{Rails.root}/config/ldap.yml"
+    config.ldap_use_admin_to_bind = true
+  end
+
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
